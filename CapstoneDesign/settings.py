b58fde98cb6de54372b63bb5232a11ca0f430e9a
Jinwon <bdcs008@mju.ac.kr>
2026-04-11 19:43:10 +0900

    CapstoneDesign/settings, urls에 course 추가 #9

--- CapstoneDesign/settings.py
+++ CapstoneDesign/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third-party
+    'rest_framework',
+    # Local
+    'courses',
 ]
 
 MIDDLEWARE = [
